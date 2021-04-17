49da0d17034c7785046aa47d59975ae309031b76
dimstar <kylermagee@gmail.com>
2021-04-17 12:08:36 -0700

    abstracts CardForm component

---
+++ pages/cc.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import CardForm from '../components/CardForm';
+
+const Cc = (): JSX.Element => (
+  <div>
+    <div>
+      <CardForm />
+    </div>
+  </div>
+);
+
+export default Cc;
